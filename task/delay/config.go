eb7181bc41190dbcfc34278b6604da5a83a7b612
xpwu <diodexp@foxmail.com>
2022-02-12 02:44:44 +0800

    update mod

--- task/delay/config.go
+++ task/delay/config.go
@@ -3,7 +3,7 @@ package delay
 import "github.com/xpwu/go-config/configs"
 
 type config struct {
-  CallbackUrl string `conf:"url, callback url"`
+  CallbackUrl string `conf:"url,callback url"`
 }
 
 var confValue = &config{}
--- task/fixed/config.go
+++ task/fixed/config.go
@@ -3,7 +3,7 @@ package fixed
 import "github.com/xpwu/go-config/configs"
 
 type config struct {
-  CallbackUrl string `conf:"url, callback url"`
+  CallbackUrl string `conf:"url,callback url"`
 }
 
 var confValue = &config{}
