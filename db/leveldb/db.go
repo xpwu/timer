2c62e4fc4e874de675d32ff3b777f34d6304a5fe
xpwu <diodexp@foxmail.com>
2022-02-12 02:56:29 +0800

    fix db path

--- db/leveldb/db.go
+++ db/leveldb/db.go
@@ -3,9 +3,11 @@ package leveldb
 import (
   "github.com/xpwu/timer/scheduler"
   "github.com/xpwu/timer/task/fixed"
+  "path/filepath"
 )
 
 func Init(root string) {
+  root = filepath.Join(root, "db")
   fixed.SetDB(newFixed(root))
   scheduler.SetDB(newScheduler(root))
 }
